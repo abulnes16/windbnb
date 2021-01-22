4f1a294ba1b3f655d91ea324ec5da716c84d164a
Angel Bulnes <angel_bulnes16@yahoo.com>
2021-01-22 09:59:15 -0600

    Agrega transiciones de componentes en formulario de busqueda

--- src/components/search-box/SearchBox.js
+++ src/components/search-box/SearchBox.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { MdAdd, MdLocationOn, MdRemove, MdSearch } from "react-icons/md";
+import { CSSTransition } from "react-transition-group";
 import "./search-box.scss";
 
 const SearchBox = ({ toggleBox, focus }) => {
@@ -32,61 +33,73 @@ const SearchBox = ({ toggleBox, focus }) => {
           />
         </div>
       </div>
-
-      {showOptions === "location" ? (
-        <ul className="search-box__locations">
-          <li>
-            <MdLocationOn className="icon" />
-            Helsinki, Finland
-          </li>
-          <li>
-            <MdLocationOn className="icon" />
-            Helsinki, Finland
-          </li>
-          <li>
-            <MdLocationOn className="icon" />
-            Helsinki, Finland
-          </li>
-          <li>
-            <MdLocationOn className="icon" />
-            Helsinki, Finland
-          </li>
-        </ul>
-      ) : (
-        <>
-          <div className="guest-counter">
-            <div>
-              <p className="guest-counter__type">Adults</p>
-              <p>Ages 13 or above</p>
+      <div className="d-flex">
+        <CSSTransition
+          in={showOptions === "location"}
+          timeout={350}
+          classNames="location-list"
+          unmountOnExit
+        >
+          <ul className="search-box__locations">
+            <li>
+              <MdLocationOn className="icon" />
+              Helsinki, Finland
+            </li>
+            <li>
+              <MdLocationOn className="icon" />
+              Helsinki, Finland
+            </li>
+            <li>
+              <MdLocationOn className="icon" />
+              Helsinki, Finland
+            </li>
+            <li>
+              <MdLocationOn className="icon" />
+              Helsinki, Finland
+            </li>
+          </ul>
+        </CSSTransition>
+        <CSSTransition
+          in={showOptions !== "location"}
+          timeout={350}
+          classNames="location-list"
+          unmountOnExit
+        >
+          <div className="counter-container">
+            <div className="guest-counter">
+              <div>
+                <p className="guest-counter__type">Adults</p>
+                <p>Ages 13 or above</p>
+              </div>
+              <div className="counter">
+                <button className="guest-counter__btn">
+                  <MdAdd />
+                </button>
+                <span>10 </span>
+                <button className="guest-counter__btn">
+                  <MdRemove />
+                </button>
+              </div>
             </div>
-            <div className="counter">
-              <button className="guest-counter__btn">
-                <MdAdd />
-              </button>
-              <span>10 </span>
-              <button className="guest-counter__btn">
-                <MdRemove />
-              </button>
+
+            <div className="guest-counter">
+              <div>
+                <p className="guest-counter__type">Children</p>
+                <p>Ages 2-12</p>
+              </div>
+              <div className="counter">
+                <button className="guest-counter__btn">
+                  <MdAdd />
+                </button>
+                <span>10 </span>
+                <button className="guest-counter__btn">
+                  <MdRemove />
+                </button>
+              </div>
             </div>
           </div>
-
-          <div className="guest-counter">
-            <div>
-              <p className="guest-counter__type">Children</p>
-              <p>Ages 2-12</p>
-            </div>
-            <div className="counter">
-              <button className="guest-counter__btn">
-                <MdAdd />
-              </button>
-              <span>10 </span>
-              <button className="guest-counter__btn">
-                <MdRemove />
-              </button>
-            </div>
-          </div>
-        </>
-      )}
+        </CSSTransition>
+      </div>
 
       <button className="search-box__btn">
         <MdSearch className="icon" />
